f2f2e3eb5c98833e9c4c4bd0bcae801b86349d33
Luan Riechi <luanriechi@alunos.utfpr.edu.br>
2023-01-06 22:34:58 -0300

    Implementando cadastro de usuario #5

--- JavaScript/CriarConta.js
+++ JavaScript/CriarConta.js
@@ -1,11 +1,45 @@
-const validarSenha = () =>{
+import { app, auth } from './config/firebase.js'
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { db } from './config/firebase.js'
+import { setDoc, doc, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
 
+
+const validarSenha = () => {
+    let nome = document.getElementById("nome").value
+    let sexo = document.querySelector('input[name="sexo"]:checked').value
+    let data = document.getElementById("data").value
+    let email = document.getElementById("email").value
     let senha = document.getElementById("senha").value
     let validasenha = document.getElementById("validaSenha").value
-    
-    if(senha != validasenha){
-        document.getElementById("error").style.display="flex"
-    } else{
-        document.getElementById("btnCadastrar").setAttribute("href","Entrar.html")
+
+    if (senha != validasenha) {
+        document.getElementById("error").style.display = "flex"
+    } else {
+
+        createUserWithEmailAndPassword(auth, email, senha)
+            .then((result) => {
+                    setDoc(doc(db, "usuarios", result.user.uid ), {
+                    nome: nome,
+                    sexo: sexo,
+                    dataNascimento: data,
+                    email: email,
+                    senha: senha
+                })
+                    .then((resultTab) => {
+                        window.location.href = "./Entrar.html"
+                    })
+                    .catch((error) => {
+                        console.log("Erro ao persistir dados: " + error)
+                    })
+
+
+            })
+            .catch((error) => {
+                console.log(error.message)
+                console.log("esta caindo aqui!!!!!!")
+            })
+
     }
 }
+
+document.getElementById("btnCadastrar").addEventListener('click', validarSenha)
--- JavaScript/config/firebase.js
+++ JavaScript/config/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { initializeFirestore } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
+const db = initializeFirestore(app, {experimentalForceLongPolling: true})
 
-export { app, auth }
+const storage = getStorage(app)
+
+export {app, auth, db, storage}
