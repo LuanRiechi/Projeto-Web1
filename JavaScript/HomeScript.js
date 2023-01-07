21327ab7691799ec2f894d036c3958657eda0a11
Luan Riechi <luanriechi@alunos.utfpr.edu.br>
2023-01-07 11:31:03 -0300

    Buscando e apresentando a lista de vacinas de um usuario #6

--- JavaScript/HomeScript.js
+++ JavaScript/HomeScript.js
@@ -3,17 +3,99 @@ import { signOut} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.
 import {db} from './config/firebase.js'
 import {query, collection, onSnapshot} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
 
+const listaVacinas = []
+
 auth.onAuthStateChanged( function(user) {
     if(!auth.currentUser){
-        window.location.href = "./Index.html"
+        window.location.href = "./index.html"
 
     } else{
 
-        const deslogar = () => {
-            signOut(auth)
+
+        const carregarVacinas = () => {
+
+            let id = auth.currentUser.uid
+            const q = query( collection(db, "usuarios/"+id+"/vacinas"))
+
+            onSnapshot(q, (results) => {
+                results.forEach((documento) => {
+                    listaVacinas.push({
+                        id: documento.id,
+                        data: documento.data().DataVacina,
+                        nome: documento.data().NomeVacina,
+                        dose: documento.data().Dose,
+                        imagem: documento.data().Comprovante,
+                        proximaDose:  documento.data().ProximaVacina,
+                    })
+                    showCardsVacinas(listaVacinas)
+                })
+      
+            })
         }
-        
-        document.getElementById("btnDeslogar").addEventListener('click', deslogar)
+
+        carregarVacinas()
+
     }
 
 })
+
+
+const showCardsVacinas = (lista) => {
+    document.getElementById("listaVacinas").innerHTML = ""
+    lista.forEach((documento) => {
+        CardVacinas(documento.nome, documento.dose, documento.data, documento.imagem, documento.proximaDose, documento.id)
+    })
+}
+
+
+
+const CardVacinas = (nome, dose, data, imagem, proximaDose,id) =>{
+
+    const DivVacina = document.createElement("article")
+
+    const NomeVacina = document.createElement("h2")
+    NomeVacina.innerHTML = nome
+
+    const DoseVacina = document.createElement("p")
+    DoseVacina.innerHTML = dose
+
+    const DataVacina = document.createElement("h3")
+    DataVacina.innerHTML = data
+
+    const ComprovanteImagem = document.createElement("img")
+    ComprovanteImagem.src = imagem
+
+    const ProxDose = document.createElement("span")
+    ProxDose.innerHTML = proximaDose
+
+    const inputHiddenId = document.createElement("input")
+    inputHiddenId.type = 'hidden'
+    inputHiddenId.hidden = id
+
+    DivVacina.appendChild(NomeVacina)
+    DivVacina.appendChild(DoseVacina)
+    DivVacina.appendChild(DataVacina)
+    DivVacina.appendChild(ComprovanteImagem)
+    DivVacina.appendChild(ProxDose)
+    DivVacina.appendChild(inputHiddenId)
+
+    DivVacina.addEventListener("click", () => {
+        window.location.href = "EditarVacinas.html?id=" + id
+    })
+
+    document.getElementById("listaVacinas").appendChild(DivVacina)
+}
+
+
+
+const deslogar = () => {
+    signOut(auth)
+
+}
+
+document.getElementById("btnDeslogar").addEventListener('click', deslogar)
+
+
+          
+
+
